e5543a08d2c6f66bb9aa9e8ffba46e2d0259470c
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-09 20:22:43 +0300

    Узнать является ли число четным

---
+++ EXPL4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Узнать является ли число четным
+Console.Write("Введите целое число ");
+int x = int.Parse(Console.ReadLine());
+double x2 = x % 2;
+if (x2==0)
+{ 
+Console.WriteLine("число четное");
+}
+else
+{
+Console.WriteLine("Число нечетное");
+Console.ReadLine();
+}
