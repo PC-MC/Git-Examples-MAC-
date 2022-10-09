4811a08326c517a213193ad8105e1d9de812d027
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-09 20:29:09 +0300

    Показать вторую цифру трёхзначного числа

---
+++ EXPL7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное целое целое число ");
+int n = int.Parse(Console.ReadLine());
+if(n > 99)
+{
+    //если ввести Console.WriteLine((n / 100) % 10); 
+    //то будет показываться первая цифра
+    Console.WriteLine("вторая цифра числа");
+    Console.WriteLine((n / 10)% 10);
+}
