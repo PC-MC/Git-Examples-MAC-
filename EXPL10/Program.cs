1c9548ce6d16e9a1ae5a971c296b93c525529fd0
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-09 20:34:18 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток

---
+++ EXPL10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine() ?? "0");
+if (x % 2 == 0)
+{
+   Console.WriteLine("{0} - кратно 2.", x);
+}
+else
+{
+    Console.WriteLine("{0} - не кратно 2.", x);
+}
