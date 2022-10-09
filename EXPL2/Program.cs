3e9c58bd891f7dd079bc93614d0a9ac3d881a382
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-09 19:49:15 +0300

    Добавил решение 2ой задачи

---
+++ EXPL2/Program.cs
@@ -0,0 +1,35 @@
+﻿//По заданному номеру дня недели вывести его название
+Console.Write("Введите день недели:");
+int n = int.Parse(Console.ReadLine()??"0");
+if (n == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (n == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (n == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (n == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (n == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (n == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (n == 7)
+{
+    Console.WriteLine("Воскресение");
+}
+else
+{
+    Console.WriteLine("Нет такого дня недели");
+}
