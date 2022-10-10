db8643ca0f16776b7fc383e1363348744311f659
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-10 14:44:09 +0300

    Узнать является ли число четным исправление ошибки

--- EXPL4/Program.cs
+++ EXPL4/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Узнать является ли число четным
 Console.Write("Введите целое число ");
-int x = int.Parse(Console.ReadLine());
+int x = int.Parse(Console.ReadLine()??"0");
 double x2 = x % 2;
 if (x2==0)
 { 
