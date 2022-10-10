e74fcabf6e34869afb8b13ebfe965f05c6bbff6d
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-10 15:24:40 +0300

    Дано число обозначающее день недели. Выяснить является номер дня недели выходным

--- Second lesson/EXPL1/Program.cs
+++ Second lesson/EXPL1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Введите день недели:");
+int n = int.Parse(Console.ReadLine() ?? "0");
+if (n == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (n == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (n == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (n == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (n == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (n == 6)
+{
+    Console.WriteLine("Суббота");
+    Console.WriteLine("Выходной день");
+}
+else if (n == 7)
+{
+    Console.WriteLine("Воскресение");
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Ошибка, в неделе всего семь дней, а вы выбрали что-то другое!");
+}
