0bc159a89aac90040b486ea51e9aa3e269508633
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-10 14:52:03 +0300

    Показать последнюю цифру трёхзначного числа исправление ошибок

--- EXPL6/Program.cs
+++ EXPL6/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Показать последнюю цифру трёхзначного числа
 Console.Write("Введите трехзначное целое целое число ");
-int n = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine()??"0");
 if(n > 99)
 {
     //если ввести Console.WriteLine((n / 100) % 10); 
