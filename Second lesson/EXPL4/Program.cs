8efb749c64967335b8ada8edb87e4116790409be
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-10 20:27:32 +0300

    EXPL4 - Найти расстояние между точками в пространстве 2D/3D

--- Second lesson/EXPL4/Program.cs
+++ Second lesson/EXPL4/Program.cs
@@ -1,1 +1,22 @@
 ﻿// Найти расстояние между точками в пространстве 2D/3D
+Double x1 = 0;
+Double x2 = 0;
+Double y1 = 0;
+Double y2 = 0;
+Double distance;
+
+Console.WriteLine("Введите координаты:");
+
+Console.Write("x1=");
+x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y1=");
+y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("x2=");
+x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y2=");
+y2 = Convert.ToDouble(Console.ReadLine());
+
+
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine("Расстояние между точками равно:" + distance);
