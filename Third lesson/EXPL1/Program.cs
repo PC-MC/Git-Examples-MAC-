d2c3f05722b30a0dea0440cf97296b1bc64e3141
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-10 20:59:24 +0300

    EXPL1 - Найти кубы чисел от 1 - N

---
+++ Third lesson/EXPL1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Сейчас поищем кубы чисел от 1 - N");
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+int index = 1;
+while (index < n)
+{
+Console.WriteLine($"Число {index} его куб равен {index*index*index}");
+index++;
+}
