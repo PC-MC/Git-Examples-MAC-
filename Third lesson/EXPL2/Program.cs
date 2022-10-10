58751eb80fa7f264849dfebafbb38a3faa5a3f18
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-10 21:42:42 +0300

    EXPL2 - подсчитать сумму цифр в числе

---
+++ Third lesson/EXPL2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Давай посчитаем сумму цифр в числе");
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int amount = 0;
+while (number > 0)
+{
+ 
+amount = amount + number % 10;
+number = number /10 ;
+
+}
+Console.WriteLine($"Сумма всех цифр числа равна: {amount}");
