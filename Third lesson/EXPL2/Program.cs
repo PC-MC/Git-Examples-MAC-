ad6d1f18815561f536f30d1722f5cc0f11be7b56
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-11 21:47:03 +0300

    Исправление ошибок

--- Third lesson/EXPL2/Program.cs
+++ Third lesson/EXPL2/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Подсчитать сумму цифр в числе
 Console.WriteLine("Давай посчитаем сумму цифр в числе");
 Console.WriteLine("Введите число:");
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine()??"0");
 int amount = 0;
 while (number > 0)
 {
- 
-amount = amount + number % 10;
-number = number /10 ;
+
+    amount = amount + number % 10;
+    number = number / 10;
 
 }
 Console.WriteLine($"Сумма всех цифр числа равна: {amount}");
