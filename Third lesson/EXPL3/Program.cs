666e669b0ec1d6c87cf30df9f8d34e841d0d79b5
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-11 23:11:34 +0300

    Написать программу вычисления произведения чисел от 1 до N

---
+++ Third lesson/EXPL3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Сейчас найдем произведение чисел от 1 до N");
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine()??"0");
+int sum = 1;
+int count = 1;
+while (count < n+1) 
+{
+   sum = sum * count;
+   count++;
+}
+ 
+Console.WriteLine($"Произведение чисел от 1 - N: {sum}");
