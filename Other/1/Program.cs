11d0e39513e453e33339ca5d44493251a39ab39f
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-11 22:38:14 +0300

    добавил папку для работ на семинаре

---
+++ Other/1/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6 };
+int n = array.Lenght;
+int find = int.Parse(Console.ReadLine()??"0");
+int index = 0;
+
+if (index < n)
+{
+     if (array[index] == find)
+     {
+        Console.WriteLine($"Число {find} имеет индекс:{index}");
+     }
+     index++;
+}
