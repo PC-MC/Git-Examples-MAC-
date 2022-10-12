ddca1810d00277e0c175512bca95886f10e10cae
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-12 21:19:19 +0300

    ввод массива с консоли

--- Other/1/Program.cs
+++ Other/1/Program.cs
@@ -1,13 +1,14 @@
-﻿int[] array = { 1, 2, 3, 4, 5, 6 };
-int n = array.Lenght;
-int find = int.Parse(Console.ReadLine()??"0");
-int index = 0;
-
-if (index < n)
+﻿Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+for (int i = 0; i < n; i++)
 {
-     if (array[index] == find)
-     {
-        Console.WriteLine($"Число {find} имеет индекс:{index}");
-     }
-     index++;
+    Console.WriteLine("Введите {0}-й элемент", i + 1);
+    a[i] = int.Parse(Console.ReadLine());
 }
+int k = 0;
+for (int i = 0; i < a.Length; i++)
+    if (a[i] % 2 == 0)
+        k++;
+Console.WriteLine(k);
+Console.ReadKey();
