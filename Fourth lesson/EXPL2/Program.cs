8723df3b98e9039b68f8625ade1fcc4c6b10dbae
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-16 12:33:28 +0300

    Задать массив из 12 элементов Найти сумму положительных/отрицательных элементов

--- Fourth lesson/EXPL2/Program.cs
+++ Fourth lesson/EXPL2/Program.cs
@@ -1,19 +1,25 @@
 ﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. 
 // Найти сумму положительных/отрицательных элементов массива
 
-Console.WriteLine("Задаем случайным образом 8 цифр в пределах 0 - 9");
-int[] myArray = new int[12];
-Random rand = new Random();
+Console.WriteLine("Задаем случайным образом 12 цифр в пределах 0 - 9");
+int[] array = new int[12];
 
-for (int x = 0; x < myArray.Length; x++)
+for (int index = 0; index < array.Length; index++)
 {
-    myArray[x] = rand.Next(0, 10);
-    Console.WriteLine($"Число {x + 1} = {myArray[x]}");
+    array[index] = new Random().Next(0, 10);
+    Console.WriteLine($"Число {index + 1} = {array[index]}");
 }
 
-int sum = 0;
-for (int i = 0; i < myArray.Length; i++)
+int sum1 = 0;
+int sum2 = 0;
+for (int index = 0; index < array.Length; index++)
 {
-    sum = sum + myArray[i];
+    if (array[index] >= 0)
+    sum1 = sum1 + array[index];
+    else
+    sum2 = sum2 + array[index];
+
 }
-Console.WriteLine($"Сумма всех чисел равна: {sum}");
+
+Console.WriteLine($"Сумма всех положительных чисел равна: {sum1}");
+Console.WriteLine($"Сумма всех отрицательных чисел равна: {sum2}");
