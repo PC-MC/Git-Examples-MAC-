bf06a27eba4cfae74374ec0d95b57084d6549105
PC-MC <114336480+PC-MC@users.noreply.github.com>
2022-10-25 21:00:57 +0300

    Добавил 7 ДЗ

---
+++ Seventh lesson/EXPL2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = i + j;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите число строк m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число столбцов n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+int[,] matrix = new int[m, n];
+FillMatrix(matrix);
+PrintMatrix(matrix);
---
+++ Seventh lesson/EXPL5/Program.cs
@@ -0,0 +1,40 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+void FillArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); 
+        }  
+    }
+} 
+void PrintArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+} 
+int SumOfDiagonal(int[,] matr)
+{
+    int sum = 0;
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        sum = sum + matr[i,i];//поскольку диагональ равна столбцу
+    }
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите кол-во строк и столбцов квадтратной матрицы ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+int[,] square = new int[k,k];
+FillArray(square);
+Console.WriteLine("Матрица:");
+PrintArray(square);
+Console.WriteLine($"Сумма элементов главной диагонали: {SumOfDiagonal(square)}");
